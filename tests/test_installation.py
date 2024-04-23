2a693a6f94701a2536617fc30f892322075f0f39
Tu T. Do <tu.dothanh1906@gmail.com>
2024-04-23 17:32:56 +0700

    u: update

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -2,19 +2,22 @@ import unittest
 from not_airflow import Task, Job
 
 
-def TestJob() -> Job:
+def build_success_job () -> Job:
+    """
+    simple graph task01 -> task02 -> task03
+    """
     with Job("test-job") as job:
         @Task.wrapper(job)
         def test01():
-            print("x")
+            pass
 
         @Task.wrapper(job)
         def test02():
-            print("y")
+            pass
 
         @Task.wrapper(job)
         def test03():
-            print("z")
+            pass
 
         # invalid job, contain cycle
         test01 >> test02
@@ -23,8 +26,52 @@ def TestJob() -> Job:
     return job
 
 
+def build_cycle_job () -> Job:
+    """
+    simple graph task01 -> task02 -> task03
+    """
+    with Job("test-job") as job:
+        @Task.wrapper(job)
+        def test01():
+            pass
+
+        @Task.wrapper(job)
+        def test02():
+            pass
+
+        @Task.wrapper(job)
+        def test03():
+            pass
+
+        # invalid job, contain cycle
+        test01 >> test02
+        test02 >> test03
+        test01 << test03
+
+    return job
+
+class SuccessTest(unittest.TestCase):
+    """
+    """
+    def setUp(self):
+        self.job_success = build_success_job()
+        self.job_fail_cycle = build_cycle_job()
+
+    def test_success(self):
+        code, msg = self.job_success()
+        self.assertEqual(code, 0)
+
+
+    def test_cycle_job (self):
+        code, msg = self.job_fail_01()
+        self.assertEqual(code, 1)
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    j = TestJob()
-    code, msg = j()
-    print(code, msg)
+    unittest.main()
+
